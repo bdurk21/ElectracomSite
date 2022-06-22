990bd0f753350fe3f2230150c4661edce7184d47
Brian Durkin Jr <tui80358@temple.edu>
2022-06-21 22:05:44 -0400

    Fixed React Bug
    Fixed a bug where nothing was showing up

---
+++ ecom-site/src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+    const [click, setClick] = useState(false);
+    return (
+        <>
+            <nav className='navbar'>
+                <div className='navbar-container'>
+                    <Link to='/' className="navbar-logo">
+                        ECOM <i className='fab fa-typo3' />
+                    </Link>
+                    <div className='menu-icon'>
+                        <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                    </div>
+                </div>
+            </nav>
+        </>
+    );
+}
+
+export default Navbar;
