c1b1e9256cd0f268a3e300dbd0ce37ec5f0b49e1
Brian Durkin Jr <tui80358@temple.edu>
2022-07-19 13:13:10 -0400

    Added Button
    Added button.js and button.css

--- ecom-site/src/components/Navbar.js
+++ ecom-site/src/components/Navbar.js
@@ -3,6 +3,10 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
     const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
     return (
         <>
             <nav className='navbar'>
@@ -10,9 +14,26 @@ function Navbar() {
                     <Link to='/' className="navbar-logo">
                         ECOM <i className='fab fa-typo3' />
                     </Link>
-                    <div className='menu-icon'>
+                    <div className='menu-icon' onClick={handleClick}>
                         <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
                     </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className='nav-item'>
+                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                                Home
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/about' className='nav-links' onClick={closeMobileMenu}>
+                                About
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to='/contact' className='nav-links' onClick={closeMobileMenu}>
+                                Contact
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
             </nav>
         </>
